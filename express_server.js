7b552cc3e94350ba22e932ba274175e25310844d
hiba02 <kangyoonsoon@hotmail.com>
2020-01-20 22:00:18 +0000

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -21,9 +21,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/set", (req, res) => {
   const a = 1;
@@ -33,3 +33,20 @@ app.get("/set", (req, res) => {
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
+
+ app.get("/urls", (req, res) => {
+  // use res.render() to pass the URL data to our template.
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+// When sending variables to an EJS template, we need to send them inside an object, even if we are only sending one variable.
+// <%= %> syntax, we tell EJS that we want the result of this code to show up on our page. I
+// <% %> <!-- This line will not show up on the page -->
+// <% if(greeting) {%>
+
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
