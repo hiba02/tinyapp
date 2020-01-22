058423a4759e34dede35a45be6d8dcb5b86f0b17
hiba02 <kangyoonsoon@hotmail.com>
2020-01-22 01:57:47 +0000

    cookies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var cookieParser = require('cookie-parser')
+app.use(cookieParser());
+
 const PORT = 8080; // default port 8080
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -40,8 +43,24 @@ app.listen(PORT, () => {
 //http://www.localhost:8080/urls
 app.get("/urls", (req, res) => {
   // use res.render() to pass the URL data to our template.
-  let templateVars = { urls: urlDatabase };
-  // console.log(templateVars);
+  let templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase 
+  };
+  console.log("req.cookies['username']", req.cookies["username"]);
+  // console.log("req.cookies['username']", req.cookies["username"]);
+  if(templateVars["username"]) {
+    console.log(templateVars["username"]);
+  } else {
+    console.log('hi');
+  }
+  // console.log(templateVars[username]);
+  // if (templateVars[username]) {
+  //   console.log(templateVars);
+  // } else if (templateVars[username] === "undefined") {
+  //   console.log("no user");
+  // }
+ 
   res.render("urls_index", templateVars);
 });
 
@@ -55,6 +74,7 @@ app.get("/urls/new", (req, res) => {
 // The captured values are populated in the req.params object, with the name of the route parameter specified in the path as their respective keys
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: 'b2xVn2',
     longURL: '9sm5xK'
   };
@@ -63,6 +83,7 @@ app.get("/urls/:shortURL", (req, res) => {
 
 app.get("/urls/:longURL", (req, res) => {
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: 'b2xVn2',
     longURL: '9sm5xK'
   };
@@ -106,9 +127,47 @@ app.post("/urls/:shortURL", (req, res) => {
   // console.log("req.params.shortURL", req.params.shortURL);
 
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
 
+
+/* 
+Login Cookies
+*/
+app.post("/login", (req, res) => {
+  console.log('req.body: ', req.body);
+  console.log('Cookies: ', res.cookies);
+  // res.cookie(req.body);
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+/* 
+Login Cookies
+*/
+// app.get('/login', function (req, res) {
+//   // Cookies that have not been signed
+//   console.log('Cookies: ', req.cookies)
+
+//   // Cookies that have been signed
+//   console.log('Signed Cookies: ', req.signedCookies)
+// })
+
+/* 
+Login Cookies
+*/
+app.post("/logout", (req, res) => {
+  // console.log('req.body: ', req.body);
+  // console.log('Cookies: ', res.cookies);
+  // // res.cookie(req.body);
+  // res.cookie('username', req.body.username);
+  console.log('before', req.body.username);
+  res.clearCookie("username");
+  console.log('after', req.body.username);
+  res.redirect('/urls');
+  
+});
