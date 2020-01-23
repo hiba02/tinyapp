2ea9ca367e284a667f2841d31d62cf6d991b6d11
hiba02 <kangyoonsoon@hotmail.com>
2020-01-23 03:43:48 +0000

    ser Registration update Header

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser')
 app.use(cookieParser());
 
 const PORT = 8080; // default port 8080
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -21,15 +21,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 }
@@ -37,9 +37,9 @@ const users = {
 
 
 //https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-const generateRandomString = function() {
-  let result           = '';
-  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const generateRandomString = function () {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let charactersLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -57,26 +57,35 @@ app.listen(PORT, () => {
 //http://www.localhost:8080/urls
 app.get("/urls", (req, res) => {
   let currentCookieID = req.cookies["username"];
-  let currentEmail = users[currentCookieID].email
-  let templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase, 
-    user: currentCookieID,
-    email: currentEmail
-  };
-  console.log('users', users)
+  console.log('urls currentCookieID', currentCookieID);
+  if (currentCookieID) {
+
+    let currentEmail = users[currentCookieID].email
+    let templateVars = {
+      username: req.cookies["username"],
+      urls: urlDatabase,
+      user: currentCookieID,
+      email: currentEmail
+    };
+    console.log('currentCookieID:  ', currentCookieID);
+    console.log("/ruls req.body ", users[currentCookieID]);
+    console.log("req.cookies[username].email", users[currentCookieID].email);
+    res.render("urls_index", templateVars);
+  } else {
+    console.log('currentCookieID: ', currentCookieID);
+    res.redirect('/register');
+  }
+  
+  //console.log('users', users)
   //console.log('users[currentCookieID].email', users[currentCookieID].email)
-  console.log('currentCookieID:  ', currentCookieID);
-  console.log("/ruls req.body ", users[currentCookieID]);
-  console.log("req.cookies[username].email", users[currentCookieID].email);
+
 
   // if(templateVars["username"]) {
   //   console.log(templateVars["username"]);
   // } else {
   //   console.log('hi');
   // }
- 
-  res.render("urls_index", templateVars);
+
 });
 
 
@@ -85,7 +94,7 @@ app.get("/urls/new", (req, res) => {
   let currentCookieID = req.cookies["username"];
   let templateVars = {
     username: req.cookies["username"],
-    urls: urlDatabase, 
+    urls: urlDatabase,
     user: currentCookieID,
     email: users[currentCookieID].email,
     shortURL: 'b2xVn2',
@@ -99,7 +108,7 @@ app.get("/urls/:shortURL", (req, res) => {
   let currentCookieID = req.cookies["username"];
   let templateVars = {
     username: req.cookies["username"],
-    urls: urlDatabase, 
+    urls: urlDatabase,
     user: currentCookieID,
     email: users[currentCookieID].email,
     shortURL: 'b2xVn2',
@@ -111,10 +120,10 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/urls/:longURL", (req, res) => {
   let templateVars = {
     username: req.cookies["username"],
-    urls: urlDatabase, 
+    urls: urlDatabase,
     user: currentCookieID,
     email: users[currentCookieID].email,
-    urls: urlDatabase, 
+    urls: urlDatabase,
     shortURL: 'b2xVn2',
     longURL: '9sm5xK'
   };
@@ -130,9 +139,9 @@ app.post("/urls", (req, res) => {
   const receivedLongURL = req.body.longURL;
   urlDatabase[randomShortURL] = receivedLongURL;
 
-  console.log("randomShortURL ",randomShortURL);
-  console.log("receivedLongURL  ",receivedLongURL);
-  console.log("urlDatabase[randomShortURL]  ",urlDatabase[randomShortURL]);
+  console.log("randomShortURL ", randomShortURL);
+  console.log("receivedLongURL  ", receivedLongURL);
+  console.log("urlDatabase[randomShortURL]  ", urlDatabase[randomShortURL]);
 
   res.redirect(`/urls`);
 });
@@ -170,17 +179,17 @@ app.post("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
+// made a comment 1.22 8:27
 /* 
-Login Cookies
+Login Cookies 
 */
-app.post("/login", (req, res) => {
-  console.log('req.body: ', req.body);
-  console.log('Cookies: ', res.cookies);
-  // res.cookie(req.body);
-  res.cookie('username', req.body.username);
-  res.redirect('/urls');
-});
+// app.post("/login", (req, res) => {
+//   console.log('req.body: ', req.body);
+//   console.log('Cookies: ', res.cookies);
+//   // res.cookie(req.body);
+//   res.cookie('username', req.body.username);
+//   res.redirect('/urls');
+// });
 
 /* 
 Login Cookies
@@ -205,7 +214,7 @@ app.post("/logout", (req, res) => {
   res.clearCookie("username");
   console.log('after', req.body.username);
   res.redirect('/urls');
-  
+
 });
 
 
@@ -213,13 +222,13 @@ app.post("/logout", (req, res) => {
 
 // register - GET
 app.get("/register", (req, res) => {
-  let templateVars = { 
+  let templateVars = {
     user: req.cookies["username"],
-    urls: urlDatabase 
+    urls: urlDatabase
   };
   console.log("req.cookies['username']", req.cookies["username"]);
 
-  if(templateVars["username"]) {
+  if (templateVars["username"]) {
     console.log(templateVars["username"]);
   } else {
     console.log('hi');
@@ -248,11 +257,11 @@ const users = {
 // send cookie with new user ID
 // if -> check email exist or not
 app.post("/register/process", (req, res) => {
-  if (!req.body.email ||!req.body.password) {
+  if (!req.body.email || !req.body.password) {
     console.log(req.body.email);
     return res.status(404).send(`<h1>Please enter email and password.</h1>`);
   }
-  
+
   let newUserID = generateRandomString();
   let newUser;
   newUser = {
@@ -260,11 +269,11 @@ app.post("/register/process", (req, res) => {
     email: req.body.email,
     password: req.body.password
   }
-  
+
   users[newUserID] = newUser;
   console.log(users);
   res.cookie('username', newUserID);
-  
+
   // for (let eachUsr in users) {
   //   console.log(users[eachUsr].email);
   //   if (users[eachUsr].email === req.body.email) {
@@ -278,35 +287,48 @@ app.post("/register/process", (req, res) => {
 
 /* login GET */
 app.get("/login", (req, res) => {
-  let currentCookieID = req.cookies["username"];
-  let currentEmail = users[currentCookieID].email;
-  console.log('currentCookieID: ', currentCookieID);
-  console.log('currentEmail: ', currentEmail);
-
-  let templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase, 
-    user: currentCookieID,
-    email: currentEmail
-  };
-
-  // if(templateVars["username"]) {
-  //   console.log(templateVars["username"]);
-  // } else {
-  //   console.log('hi');
-  // }
-  res.render("urls_login", templateVars);
+  
+  
+  if (req.cookies["username"]) {
+    const currentCookieID = req.cookies["username"];
+    
+    console.log('login currentCookieID', currentCookieID);
+    console.log('login users', users);
+    const currentEmail = users[currentCookieID].email
+    // let newUserID = generateRandomString();
+    // let newUser;
+    // newUser = {
+    //   id: newUserID,
+    //   email: req.body.email,
+    //   password: req.body.password
+    // }
+    
+    const templateVars = {
+      username: req.cookies["username"],
+      user: currentCookieID,
+      email: currentEmail
+    };
+    console.log('login currentCookieID', currentCookieID);
+    console.log('currentCookieID:  ', currentCookieID);
+    console.log("/login req.body ", users[currentCookieID]);
+    console.log("req.cookies[username].email", users[currentCookieID].email);
+    console.log("/login templateVar", templateVars);
+    res.render("urls_login", templateVars);
+  } else {
+    console.log('login GET else req.cookies["username"]: ', req.cookies["username"]);
+    res.redirect("urls_login");
+  }
 });
 
 
 
 /* login POST */
 app.post("/login", (req, res) => {
-  if (!req.body.email ||!req.body.password) {
+  if (!req.body.email || !req.body.password) {
     console.log(req.body.email);
     return res.status(404).send(`<h1>Please enter email and password.</h1>`);
   }
-  
+
   let newUserID = generateRandomString();
   let newUser;
   newUser = {
@@ -314,11 +336,11 @@ app.post("/login", (req, res) => {
     email: req.body.email,
     password: req.body.password
   }
-  
+
   users[newUserID] = newUser;
   console.log(users);
   res.cookie('username', newUserID);
-  
+
   // for (let eachUsr in users) {
   //   console.log(users[eachUsr].email);
   //   if (users[eachUsr].email === req.body.email) {
